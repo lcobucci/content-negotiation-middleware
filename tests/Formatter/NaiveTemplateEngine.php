634f10dd574f34250cf95ced62f5c027025c6804
Luís Cobucci <lcobucci@gmail.com>
2018-04-22 11:49:09 +0200

    Make middleware pass the response attributes to formatter

---
+++ tests/Formatter/NaiveTemplateEngine.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+
+namespace Lcobucci\ContentNegotiation\Tests\Formatter;
+
+use Lcobucci\ContentNegotiation\Formatter;
+use SplFileObject;
+use function array_keys;
+use function array_map;
+use function assert;
+use function is_string;
+use function str_replace;
+use function trim;
+
+final class NaiveTemplateEngine implements Formatter
+{
+    private const BASE_DIR  = __DIR__ . '/templates/naive/';
+    private const EXTENSION = 'html';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function format($content, array $attributes = []): string
+    {
+        $template = $this->getTemplateContent($attributes);
+
+        return $this->render($template, (array) $content);
+    }
+
+    /**
+     * @param mixed[] $attributes
+     */
+    private function getTemplateContent(array $attributes): string
+    {
+        $template = $attributes['template'] ?? '';
+        assert(is_string($template));
+
+        $file = new SplFileObject(self::BASE_DIR . $template . '.' . self::EXTENSION);
+
+        return $file->fread($file->getSize());
+    }
+
+    /**
+     * @param mixed[] $data
+     */
+    private function render(string $template, array $data): string
+    {
+        $variables = array_map(
+            function (string $attribute): string {
+                return '{' . $attribute . '}';
+            },
+            array_keys($data)
+        );
+
+        return trim(str_replace($variables, $data, $template));
+    }
+}
